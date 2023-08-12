a22ae16f5b7f5c0e42d12f71d006f30b0799618d
Svengadesh <Svengadesh.subramani@gmail.com>
2023-08-12 13:28:38 +0530

    Add project files.

---
+++ HTMLMVC Link/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HTMLMVC_Link.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
